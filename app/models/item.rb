c761467d9a225a6e975748bec3985ddbbe4053d3
Alfred Osorio <alfred.osorio@outlook.com>
2017-05-12 00:14:28 +1000

    fix s3 heroku img bug

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
-  has_attached_file :item_image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png",
-  path: ":filename"
+  has_attached_file :item_image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :item_image, content_type: /\Aimage\/.*\z/
 
   has_many :bids
