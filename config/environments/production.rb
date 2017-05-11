500f0466c58e208a895f8a896b394c3e3af2c462
Alfred Osorio <alfred.osorio@outlook.com>
2017-05-11 22:27:38 +1000

    create paperclip initializer to support int'l url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings
 
   # Paperclip - AWS Configs
-  config.paperclip_defaults = { s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com", }
+  config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
